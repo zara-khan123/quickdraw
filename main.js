200bd42db15fb0cab17eb8b0151bec5a7a616081
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-03-31 20:25:11 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -7,13 +7,18 @@ var answer_holder="";
 var score=0;
 console.log("1")
 
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
 function updatecanvas()
 {
     background("white");
     random_number = Math.floor((Math.random()*quick_draw_data_set.length)+1);
     console.log(quick_draw_data_set[random_number]);
     sketch = quick_draw_data_set[random_number];
-    document.getElementById("draw").innerHTML = "sketch to be drawn" + sketch;
+    document.getElementById("draw").innerHTML = "sketch to be drawn  " + sketch;
 
 }
 
@@ -22,6 +27,7 @@ function setup()
     canvas = createCanvas(280, 280);
     canvas.center();
     background("white");
+    classifyCanvas();
 }
 
 function draw()
@@ -33,12 +39,18 @@ function draw()
         score++;
         document.getElementById("score").innerHTML = "Score: "+score;
     }
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed)
+    {
+       line(pmouseX, pmouseY, mouseX, mouseY);
+    }
 }
 
 function check_sketch()
 {
     timer_counter++;
-    document.getElementById("time").innerHTML = "Timer"+timer_counter;
+    document.getElementById("time").innerHTML = "Timer "+timer_counter;
     console.log(timer_counter);
     if(timer_counter > 400)
     {
@@ -51,4 +63,21 @@ function check_sketch()
     answer_holder = "";
     updatecanvas();
     }
+}
+
+function  classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+
+console.log(results);
+document.getElementById('label').innerHTML = 'label:  ' + results[0].label;
+document.getElementById('confidence').innerHTML = 'Confidence:  ' + Math.round(results[0].confidence * 100) + '%';
+utterThis = new SpeechSynthesisUtterance(results[0].label);
+synth.speak(utterThis);
 }
